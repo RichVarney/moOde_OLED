8a4560311223db090332ba0d570e3a4443ab3642
RichVarney <35279192+RichVarney@users.noreply.github.com>
2021-06-03 11:58:13 +0100

    Update install_moOde_OLED.sh
    Removing rc.local method of installation

--- install_moOde_OLED.sh
+++ install_moOde_OLED.sh
@@ -5,9 +5,6 @@ echo "****              MOODE               ****"
 echo "****                                  ****"
 echo "******************************************"
 echo "******************************************"
-echo "the below rc.local method doesn't work"
-echo "use /etc/systemd/system instead"
-sudo sed -i "`wc -l < /etc/rc.local`i\\sudo python3 /opt/moOde_OLED/moOde_OLED.py &\\" /etc/rc.local
 sudo cp /opt/moOde_OLED/moode-oled.service /etc/systemd/system/
 sudo systemctl enable moode-oled.service
 echo "******************************************"

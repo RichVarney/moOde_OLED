73a40a60181efefcd38d0fdb374b59e7e034a411
RichVarney <35279192+RichVarney@users.noreply.github.com>
2021-06-03 12:26:13 +0100

    Update install_moOde_OLED.sh
    might need to add 'sudo systemctl start moode-oled.service'

--- install_moOde_OLED.sh
+++ install_moOde_OLED.sh
@@ -7,6 +7,7 @@ echo "******************************************"
 echo "******************************************"
 sudo cp /opt/moOde_OLED/moode-oled.service /etc/systemd/system/
 sudo systemctl enable moode-oled.service
+echo "might need to add 'sudo systemctl start moode-oled.service'"
 echo "******************************************"
 echo "***      Successfully Installed        ***"
 echo "******************************************"

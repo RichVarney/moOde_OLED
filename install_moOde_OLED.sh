945a49b1801c37a29b5776deaaf33a147de43664
RichVarney <35279192+RichVarney@users.noreply.github.com>
2021-06-03 11:55:20 +0100

    Update install_moOde_OLED.sh
    Forgot to enable the service
    "sudo systemctl enable moode-oled.service"

--- install_moOde_OLED.sh
+++ install_moOde_OLED.sh
@@ -9,6 +9,7 @@ echo "the below rc.local method doesn't work"
 echo "use /etc/systemd/system instead"
 sudo sed -i "`wc -l < /etc/rc.local`i\\sudo python3 /opt/moOde_OLED/moOde_OLED.py &\\" /etc/rc.local
 sudo cp /opt/moOde_OLED/moode-oled.service /etc/systemd/system/
+sudo systemctl enable moode-oled.service
 echo "******************************************"
 echo "***      Successfully Installed        ***"
 echo "******************************************"
